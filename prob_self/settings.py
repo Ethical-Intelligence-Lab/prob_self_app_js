b77eb7436539824f9ce63f46b51ce91498424fcc
akaanug <akaanug@gmail.com>
2021-02-04 00:28:38 +0300

    update contingency

--- prob_self/settings.py
+++ prob_self/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'mrb+w6fx+f5^t356e=1^nwou0_z1g(ked^cyevj+f^ug#nb0f!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['prob-self-mturk.herokuapp.com', '127.0.0.1']
 

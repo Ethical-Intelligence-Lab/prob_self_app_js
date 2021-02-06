604d1b43ac8e826cc0f53380c768ece98327850a
akaanug <akaanug@gmail.com>
2021-02-06 20:16:18 +0300

    fix

--- game/mturk.py
+++ game/mturk.py
@@ -30,10 +30,10 @@ def connect_mturk():
 
 
 def create_task(client):
-    question = ExternalQuestion("https://prob-self-mturk.herokuapp.com/", frame_height=600)
+    question = ExternalQuestion("https://prob-self-mturk.herokuapp.com/", frame_height=540)
     new_hit = client.create_hit(
         Title='Finish the game.',
-        Description='Move with WASD or arrow keys.',
+        Description=' Move with WASD keys. It might take approximately 30 secs to load the page.',
         Keywords='question, answer, research, game',
         Reward='0.15',
         MaxAssignments=100,

144987cc6c80361f2b4a767a6caf2d19354da9a1
akaanug <46591206+akaanug@users.noreply.github.com>
2021-12-02 20:34:06 +0300

    Update settings.py

--- prob_self/settings.py
+++ prob_self/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['prob-self-mturk.herokuapp.com', '127.0.0.1', '8bda-5-25-151-89.ngrok.io']
+ALLOWED_HOSTS = ['prob-self-mturk.herokuapp.com']
 
 # Application definition
 

47bad51ab3e516465e6f4f983aebd3ff9586d353
Kaan Uğuralp <akaanug@gmail.com>
2023-04-27 20:40:30 +0300

    update level amount

--- staticfiles/js/Environment.js
+++ staticfiles/js/Environment.js
@@ -220,7 +220,7 @@ class Game {
     #mockSelf;
 
     constructor(gameType) {
-        this.#num_levels = 50;
+        this.#num_levels = 54;
         this.#gameType = gameType;
         this.#level_count = 0;
         this.#avatarPosition = random_avatar_pos(gameType);
@@ -247,7 +247,7 @@ class Game {
             this.#board = JSON.parse(JSON.stringify(this.#possible_levels[0]));
 
             if (gameType === "change_agent_perturbed" || gameType === "contingency_perturbed") {  // Construct the mock self
-                this.#num_levels = 50;
+                this.#num_levels = 54;
             }
         }
     }
@@ -341,7 +341,7 @@ class Game {
 
             if (this.#gameType === "change_agent_perturbed" || this.#gameType === "contingency_perturbed") {
                 // Add the mock self
-                if (this.getLevelCount() > 24) {
+                if (this.getLevelCount() > 33) {
                     this.#mockSelf = new MockSelf(this.#gameType, random_avatar_pos(this.#gameType, true));
 
                     // Set mock self's position on the board
@@ -432,7 +432,7 @@ class Game {
         }
 
         if ((this.#gameType === 'change_agent_perturbed') || (this.#gameType === 'contingency_perturbed')) { // Move mock self as well, if it exists
-            if (this.getLevelCount() > 24) {
+            if (this.getLevelCount() > 33) {
                 this.#mockSelf.move(this.getBoard());
             }
         }
@@ -490,7 +490,7 @@ class Game {
         }
 
         if ((this.#gameType === 'change_agent_perturbed') || (this.#gameType === 'contingency_perturbed')) { // Move mock self as well, if it exists
-            if (this.getLevelCount() > 24) {
+            if (this.getLevelCount() > 33) {
                 this.#mockSelf.move(this.getBoard());
             }
         }

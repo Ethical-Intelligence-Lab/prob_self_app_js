21bf25069a131a708f442111e61ed24eafc66909
Kaan Uğuralp <akaanug@gmail.com>
2023-04-28 03:02:11 +0300

    update upload mem

--- prob_self/settings.py
+++ prob_self/settings.py
@@ -32,7 +32,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['eillab.xyz']
 
 # Application definition
-DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
+DATA_UPLOAD_MAX_MEMORY_SIZE = 20971520
 
 INSTALLED_APPS = [
     'django.contrib.admin',
